da71bc1c9560d421733f48324c2f42eed73d9bc0
Jacob Kolding <dacobi@gmail.com>
2024-03-11 19:51:03 +0100

    First test 13

--- x16ppd.cpp
+++ x16ppd.cpp
@@ -162,7 +162,7 @@ unsigned char PPort::read(){
         mPPd.throwError(PPDERR_FATAL_IO, "I2C IO Error!");        
     }
 
-    return mRead[1];
+    return mRead[0];
 }
 
 void PPort::write(unsigned char cByte){
